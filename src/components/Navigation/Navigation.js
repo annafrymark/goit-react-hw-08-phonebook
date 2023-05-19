5ac691c741ee955eb6c0ea94012cb4c81ac12bfd
annafrymark <anna.frymark@gmail.com>
2023-05-19 15:12:51 +0200

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import { useAuth } from '../../hooks/useAuth';
 import css from './Navigation.module.css';
 
 export const Navigation = () => {
-  const { isLoggedIn } = useAuth;
+  const { isLoggedIn } = useAuth();
 
   return (
     <nav>
