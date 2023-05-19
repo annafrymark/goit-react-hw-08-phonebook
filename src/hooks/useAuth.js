73cc227431897501e0d35f8f03b481bd7056318f
annafrymark <anna.frymark@gmail.com>
2023-05-19 12:45:17 +0200

    selectors update

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,18 +1,18 @@
 import { useSelector } from 'react-redux';
 import {
-  selectError,
   selectIsLoggedIn,
   selectUser,
+  selectIsRefreshing,
 } from '../redux/auth/selectors';
 
 export const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
   const user = useSelector(selectUser);
-  const error = useSelector(selectError);
 
   return {
     isLoggedIn,
+    isRefreshing,
     user,
-    error,
   };
 };
--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -2,6 +2,4 @@ export const selectUser = state => state.auth.user;
 
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
-export const selectError = state => state.auth.error;
-
 export const selectIsRefreshing = state => state.auth.isRefreshing;
